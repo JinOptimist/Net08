d8e753100c059c1cac1824642cd110c98a60253b
JinOptimist <88jin@mail.ru>
2021-07-23 20:45:41 +0300

    Visualize maze

---
+++ Net08/WebMazeMvc/Models/MazeDrawViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMazeMvc.Models
+{
+    public class MazeDrawViewModel
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string[,] Cells { get; set; } 
+    }
+}
