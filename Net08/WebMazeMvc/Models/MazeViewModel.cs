0b06d742902b9f0352bf0379d2450ec9ad93af8c
JinOptimist <88jin@mail.ru>
2021-07-23 21:15:31 +0300

    Add display name

--- Net08/WebMazeMvc/Models/MazeViewModel.cs
+++ Net08/WebMazeMvc/Models/MazeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,12 +11,15 @@ namespace WebMazeMvc.Models
     public class MazeViewModel
     {
         [Required]
+        [DisplayName("Название лабиринта")]
         public string Title { get; set; }
 
         [Min(4)]
+        [DisplayName("Ширина лабиринта")]
         public int Width { get; set; }
 
         [Min(3)]
+        [DisplayName("Высота лабиринта")]
         public int Height { get; set; }
     }
 }
