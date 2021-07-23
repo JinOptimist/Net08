990c08a221d48f5ab993ec4b0894cc66b4cb65ae
JinOptimist <88jin@mail.ru>
2021-07-23 19:41:35 +0300

    Add custome validation attribute

---
+++ Net08/WebMazeMvc/Models/MazeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMazeMvc.Models.CustomValidationAttribute;
+
+namespace WebMazeMvc.Models
+{
+    public class MazeViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Min(4)]
+        public int Width { get; set; }
+
+        [Min(3)]
+        public int Height { get; set; }
+    }
+}
