0e46030121824ea9c3911ef4b7cb3a56d1cb9062
JinOptimist <88jin@mail.ru>
2021-07-30 19:31:26 +0300

    Add user repository

---
+++ Net08/WebMazeMvc/EfStuff/Repositories/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMazeMvc.EfStuff.Model;
+
+namespace WebMazeMvc.EfStuff.Repositories
+{
+    public class UserRepository
+    {
+        private MazeDbContext _mazeDbContext;
+
+        public UserRepository(MazeDbContext dbContext)
+        {
+            _mazeDbContext = dbContext;
+        }
+
+        public User Get(long id)
+        {
+            return _mazeDbContext.Users.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<User> GetAll()
+        {
+            return _mazeDbContext.Users.ToList();
+        }
+
+        public void Save(User user)
+        {
+            if (user.Id > 0)
+            {
+                _mazeDbContext.Update(user);
+            }
+            else
+            {
+                _mazeDbContext.Users.Add(user);
+            }
+
+            _mazeDbContext.SaveChanges();
+        }
+
+        public void Remove(User user)
+        {
+            _mazeDbContext.Users.Remove(user);
+            _mazeDbContext.SaveChanges();
+        }
+    }
+}
