31b4e4dae6d264dd0782d78b709458dbc808920b
lyucifer97@list.ru <lyucifer97@list.ru>
2021-07-30 17:35:30 +0300

    Task9

---
+++ Net08/WebMazeMvc/Controllers/NewsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMazeMvc.EfStuff;
+using WebMazeMvc.EfStuff.Model;
+using WebMazeMvc.Models;
+
+namespace WebMazeMvc.Controllers
+{
+    public class NewsController: Controller
+    {
+        private MazeDbContext _mazeDbContext;
+
+        public NewsController(MazeDbContext mazeDbContext)
+        {
+            _mazeDbContext = mazeDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult AllNews()
+        {
+            var allUsers = _mazeDbContext.News.ToList();
+
+            var viewModels = allUsers
+                .Select(x => new AddNewsViewModel()
+                {
+                     Title= x.Title,
+                     Source= x.Source
+                }).ToList();
+            return View(viewModels);
+        }
+
+        [HttpPost]
+        public IActionResult AddNews(AddNewsViewModel viewModel)
+        {
+            var news = new News()
+            {
+                Title = viewModel.Title,
+                Source = viewModel.Source
+            };
+
+            _mazeDbContext.News.Add(news);
+
+            _mazeDbContext.SaveChanges();
+
+            return RedirectToAction("All", "News");
+        }
+
+        public IActionResult Remove(AddNewsViewModel addNewsViewModel)
+        {
+
+            var news = _mazeDbContext
+                .News
+                .Single(x => x.Id == addNewsViewModel.Id);
+
+            _mazeDbContext.News.Remove(news);
+            _mazeDbContext.SaveChanges();
+
+            return RedirectToAction("All", "News");
+        }
+    }
+}
---
+++ Net08/WebMazeMvc/Models/AddNewsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMazeMvc.Models
+{
+    public class AddNewsViewModel
+    {
+        public string Title { get; set; }
+
+        public string Source { get; set; }
+
+        public long Id { get; set; }
+    }
+}
