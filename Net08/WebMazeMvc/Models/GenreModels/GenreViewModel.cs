43a5f4b2b07dd758a5a4793d1dedba34bc43e4e4
DmitriyHolub <6367394@mail.ru>
2021-08-06 17:11:54 +0300

    1

--- Net08/WebMazeMvc/Models/GenreModels/GenreViewModel.cs
+++ Net08/WebMazeMvc/Models/GenreModels/GenreViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace WebMazeMvc.Models
     public class GenreViewModel
     {
         public long Id { get; set; }
+        [Required (ErrorMessage ="Введите название жанра") ]
         public string NameGenre { get; set; }
     }
 }
