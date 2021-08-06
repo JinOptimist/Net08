e82db65b83ba44b9fd7f3feadc2b130a19bafcd7
JinOptimist <88jin@mail.ru>
2021-08-06 21:46:54 +0300

    Add CustomAuthSAttribute and Role

---
+++ Net08/WebMazeMvc/EfStuff/Model/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMazeMvc.EfStuff.Model
+{
+    public enum Role
+    {
+        User = 1,
+        Moderator = 3,
+        Admin = 99
+    }
+}
