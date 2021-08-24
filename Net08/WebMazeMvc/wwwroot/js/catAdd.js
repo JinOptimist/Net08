00d43d5a968bb8863478774cd5eea72bcae053b8
yuramazurek <yura-mazurek@rambler.ru>
2021-08-24 20:40:57 +0300

    fixed add cat

--- Net08/WebMazeMvc/wwwroot/js/catAdd.js
+++ Net08/WebMazeMvc/wwwroot/js/catAdd.js
@@ -1,23 +1,24 @@
 ﻿$(document).ready(function () {
+    $('.icon').hide();
 
     $('input[name=name]').keyup(function (e) {
         var self = $(this);
         var currentName = self.val();
 
         var url = '/cat/IsUniq?name=' + currentName;
-        $.get(url)
-            .done(function (respone) {
+        var promise = $.get(url);
+        promise.done(function (respone) {
+            $('.icon').hide();                   
                 if (respone) {
-                    self.css('border-color', 'green');
+                    $('.icon.ok').show();
+                    /*self.css('border-color', 'green');*/
                 } else {
-                    self.css('border-color', 'red');
+                    $('.icon.cancel').show();
+                    //self.css('border-color', 'red');
                 }
             });
-
-        //$('body').show();
-        //$('body').hide();
     });
 
-
-
+    //$('body').show();
+    //$('body').hide();
 });
